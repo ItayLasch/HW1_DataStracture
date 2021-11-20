a305b34908e5bc0a85dcf4782e7ed03ebe73fb44
ItayLasch <itay.lasch@campus.technion.ac.il>
2021-11-20 21:29:32 +0200

    all

--- AVLTree.h
+++ AVLTree.h
@@ -8,78 +8,37 @@ class AVLTree
     template<class T>
     class Node
     {
-        T data;
-        Node<T> *left;
-        Node<T> *right;
-        Node<T> *parent;
-        int height;
+        private:
+            T data;
+            Node<T> *left;
+            Node<T> *right;
+            Node<T> *parent;
+            int height;
 
-        Node(T data) : data(data), left(nullptr), right(nullptr), parent(nullptr){}
-        ~Node() = default;
-        Node(const Node& other) = default;
-        Node& operator=(const Node& other) = default;
-        T getDataNode(){
-            return this->data;
-        }
-        friend class AVLTree;
-    };
-   
-    Node<T> root;
-    int size;
+            Node(T data) : data(data), left(nullptr), right(nullptr), parent(nullptr){}
+            ~Node() = default;
+            Node(const Node& other) = default;
+            Node& operator=(const Node& other) = default;
+            T getDataNode(){
+                return this->data;
+            }
+            friend class AVLTree;
+        };
     
-    public:
-        AVLTree(): root(nullptr), size(0){};
-        ~AVLTree() = default;
-        AVLTree(const AVLTree& other): root(other->root), size(other->size){
-            copy_tree(other->root, nullptr);
+        Node<T> root;
+        int size;
+    
+        void replaceNodes(Node<T>* node1, Node<T>* node2){
+            Node<T>* temp = node1;
+            node1->parent = node2->parent;
+            node1->left = node2->left;
+            node1->right = node2->right;
+            node2->parent = temp->parent;
+            node2->left = temp->left;
+            node2->right = temp->right;
         }
 
-        
-        void AddItem(const T new_data){
-            if(this->root == nullptr){
-                root = Node(new_data);
-                this->size++;
-            }
-            else
-                this->root->AddItemAux(new_data);
-            //add roll functions*******************************************************************************************************
-        }
-
-
-       
-        void removeItem(int id){
-            if(!this->isExists(id)){
-                throw PlayerNotExsist();
-            }
-            Node<T> node_to_remove = FindItem(id);
-            if(node_to_remove->left == nullptr){
-                node_to_remove->right->parent = node_to_remove->parent;
-                node_to_remove->parent
-            }
-            
-        }
-
-        bool isExists(int id){
-            if(this->FindItem(id) == nullptr){
-                return false;
-            }
-            return true;
-        }
-
-
-        T getData(int id)
-        {
-            Node<T> *temp = FindItem(id);
-            if(temp == nullptr)
-                return nullptr;
-
-            return temp->getDataNode();
-        }
-        
-        class PlayerNotExsist(): public std::exception
-
-    private:
-        //log(n- size of the tree)
+        //O(logn) n - size of the tree
         Node<T> copy_tree(Node<T> other, Node<T> parent){
             if(other == nullptr){
                 return nullptr;
@@ -91,7 +50,7 @@ class AVLTree
             newNode->right = copy_tree(other->right, new_node);
             return new_node;
         }
-        //log(n- size of the tree)
+        //O(logn) n - size of the tree
         //return the node
         Node<T> FindItem(int id){
             if(this == nullptr){ return nullptr;}
@@ -101,17 +60,21 @@ class AVLTree
             }
             return this->right->FindItem(id);
         }
-        //log(n- size of the tree)
+        //O(logn) n - size of the tree
         //return the node
         Node<T> FindItem(int level, int id){
             if(this == nullptr){ return nullptr;}
             if(this->data->getID() == id){return this;}
-            if(this->data->level > level || (this->data->level == level && this->data->level > id)){
+            if(this->data->level > level || (this->data->level == level && this->data->id > id)){
                 return this->left->FindItem(level, id));
             }
-            return this->right->FindItem(id);
+            else{
+                return this->right->FindItem(id);
+            }
         }
-        //log(n- size of the tree)
+
+        
+        //O(logn) n - size of the tree
          void AddItemAux(new_data){
             if(this->left == nullptr && this->right == nullptr){
                 if(this->data->id > new_data->id){
@@ -127,9 +90,100 @@ class AVLTree
             }
             this->heigth++;
         }
+        //get the min Node. O(logn), n-size of the tree
+        Node<T>* NodeToReplace(){
+            if(this->left == nullptr){
+                return this;
+            }
+            this->left->NodeToReplace();
+        }
 
+    public:
 
+        AVLTree(): root(nullptr), size(0){};
+        ~AVLTree() = default;
+        AVLTree(const AVLTree& other): root(other->root), size(other->size){
+            copy_tree(other->root, nullptr);
+        }
 
+        
+        void AddItem(const T new_data){
+            if(this->root == nullptr){
+                root = Node(new_data);
+                this->size++;
+            }
+            else
+                this->root->AddItemAux(new_data);
+                this->size++;
+            //add roll functions*******************************************************************************************************
+        }
+
+        void removeItem(int id){
+            if(!this->isExists(id)){
+                throw PlayerNotExsist();
+            }
+            Node<T>* node_to_remove = FindItem(id);
+            if(node_to_remove->left == nullptr){
+                node_to_remove->right->parent = node_to_remove->parent;
+                if(node_to_remove == node_to_remove->parent->left){
+                    node_to_remove->parent->left = node_to_remove->right;
+                }
+                else{
+                    node_to_remove->parent->right = node_to_remove->right;
+                }
+            }
+            elif(node_to_remove->right == nullptr){
+                node_to_remove->left->parent = node_to_remove->parent;
+                if(node_to_remove == node_to_remove->parent->left){
+                    node_to_remove->parent->left = node_to_remove->left;
+                }
+                else{
+                    node_to_remove->parent->right = node_to_remove->left;
+                }
+            }
+            else{
+                Node<T>* temp = NodeToReplace(node_to_remove->right);
+                replaceNodes(temp, node_to_remove);
+                if(node_to_remove->right != nullptr){
+                    node_to_remove->right = node_to_remove->parent;
+                }
+                node_to_remove->parent->left = node_to_remove->right;
+            }
+            delete(node_to_remove);
+            this->size--;
+            //add roll functions**********************************************************************************************************
+        }
+
+        bool isExists(int id){
+            if(this->FindItem(id) == nullptr){
+                return false;
+            }
+            return true;
+        }
+
+        T getData(int id)
+        {
+            Node<T> *temp = FindItem(id);
+            if(temp == nullptr)
+                return nullptr;
+
+            return temp->getDataNode();
+        }
+        //O(logn) n-number of players in the group
+        T getMax(){
+            if(this->root == nullptr){
+                throw EmptyDataSystem;
+            }
+            Node<T>* temp = this->root;
+            while(temp->right != nullptr){
+                temp = temp->right;
+            }
+            return temp->data;
+        }
+        class EmptyDataSystem(): public std::exception
+        class PlayerNotExsist(): public std::exception
+
+    
 
 };
 
